adb64f2065fa7e809c6eaee75975b747764e5e11
loredanabudani <loredanabudani1@gmail.com>
2026-02-04 18:03:42 +0100

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,1 +1,17 @@
+-- Female employees working in departments located in Fier
+SELECT p.emri
+FROM Punonjes p
+JOIN Departament_Lokacion dl ON p.numri = dl.numri
+WHERE p.gjinia = 'F'
+AND dl.departlokacion = 'Fier';
+
+-- Employees working 21 hours per week
+SELECT SSN
+FROM Punon_Ne
+WHERE ore_pune = 21;
+
+-- Employees and projects with 11 working hours
+SELECT projektnumri, SSN
+FROM Punon_Ne
+WHERE ore_pune = 11;
 
