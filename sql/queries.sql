3d590bfa24d11a2a3dfa012f689c7aa4ba5824d3
loredanabudani <loredanabudani1@gmail.com>
2026-02-04 20:58:18 +0100

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,3 +1,41 @@
+-- ===============================
+-- COMPANY DATABASE PROJECT
+-- ===============================
+
+-- ===============================
+-- CREATE TABLE STATEMENTS
+-- ===============================
+-- (këtu vendoset kodi CREATE TABLE që kemi ndërtuar më parë)
+
+-- ===============================
+-- INSERT SAMPLE DATA
+-- ===============================
+-- (këtu vendosen INSERT INTO ...)
+
+-- ===============================
+-- BASIC SELECT QUERIES
+-- ===============================
+
+-- Employees and their departments
+SELECT p.emri, d.departemri
+FROM Punonjes p
+JOIN Departament d ON p.numri = d.numri;
+
+-- Projects and their departments
+SELECT pr.projektemri, d.departemri
+FROM Projekt pr
+JOIN Departament d ON pr.numri = d.numri;
+
+-- Employees working on projects with hours
+SELECT p.emri, pr.projektemri, pn.ore_pune
+FROM Punon_Ne pn
+JOIN Punonjes p ON pn.SSN = p.SSN
+JOIN Projekt pr ON pn.projektnumri = pr.projektnumri;
+
+-- ===============================
+-- ADVANCED QUERIES
+-- ===============================
+
 -- Female employees working in departments located in Fier
 SELECT p.emri
 FROM Punonjes p
@@ -14,25 +52,3 @@ WHERE ore_pune = 21;
 SELECT projektnumri, SSN
 FROM Punon_Ne
 WHERE ore_pune = 11;
-
-
-
--- CREATE TABLE statements
--- (këtu vjen kodi i tabelave)
-
--- INSERT sample data
--- (këtu vjen INSERT)
-
--- SELECT queries
-SELECT p.emri, d.departemri
-FROM Punonjes p
-JOIN Departament d ON p.numri = d.numri;
-
-SELECT pr.projektemri, d.departemri
-FROM Projekt pr
-JOIN Departament d ON pr.numri = d.numri;
-
-SELECT p.emri, pr.projektemri, pn.ore_pune
-FROM Punon_Ne pn
-JOIN Punonjes p ON pn.SSN = p.SSN
-JOIN Projekt pr ON pn.projektnumri = pr.projektnumri;
